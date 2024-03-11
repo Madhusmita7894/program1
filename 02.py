d11406f52315c78571b6c493f7c3ac556aac9db1
Madhusmita Barik <162332770+Madhusmita7894@users.noreply.github.com>
2024-03-11 09:55:30 -0700

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,73 @@
+# #bool type
+# #a=True
+# #print(type(a))
+# #print(a)
+# #string slicing and indexing
+# a=50
+# b=60
+# c=a>b
+# print(c)
+# str="welcome to nalanda"
+# print(str[4])
+# print(str[-15])
+# print(str[0:8:1])
+# print(str[0:8:2])
+# print(str[1:12:2])
+# print(str[3:15:2])
+# print(str[::2])
+# print(str[:5000:2])
+# print(str[100:12:2])
+# print(str[::-1])
+# print(str[::-2])
+# print(str[-15:-14:-1])
+# print(str[-12:2:-1])
+# print(str[8:15:-1])
+# print(str[5000:5:-1])
+# print(str[-6: :])
+# print(str[-1:-4:-1])
+# print(str[-4:-1: ])
+# print(str[:-4:])
+# print(str[2:9])
+# print(str[1:-12:])
+# #type conversion
+# #type casting:-convert one data to another data
+# #implicit
+# a=100
+# b=3
+# c=a/b
+# print(c)
+# print(type(c))
+# a=50
+# b=60.2
+# c=a+b
+# print(c)
+# #float to int
+# a=int(55.78)
+# print(a)
+# #a=int(23+78j)
+# #print(a)
+# #a=int(True)
+# #print(a)
+
+# a=int("100")
+# print(a)
+# #other to float
+# a=float(10)
+# print(a)
+# #boolean to float
+# a=float(True)
+# print(a)
+# a=float("600")
+# print(a)
+# a=complex(24.3)
+# print(a)
+# a=complex("60")
+# print(a)
+# #int to bool
+# a=bool(12)
+# print(a)
+# a=bool(0)
+# print(a)
+ #float to bool
+ a=bool(12.23)
+ print(a)
